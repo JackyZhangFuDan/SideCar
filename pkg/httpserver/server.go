e8fd1df474837bb61885dba7c3a76ba3c4410667
Jacky Zhang <jacky01.zhang@outlook.com>
2022-07-05 21:55:59 +0800

    enhance server

--- pkg/httpserver/server.go
+++ pkg/httpserver/server.go
@@ -1,9 +1,13 @@
 package httpserver
 
 import (
+	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
+
+	"github.com/jackyzhangfudan/sidecar/pkg/ca"
 )
 
 var server *http.Server
@@ -19,6 +23,7 @@ func Run() {
 
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", rootHandler)
+	mux.HandleFunc("/csr", signCsr)
 	server = &http.Server{
 		Addr:    ":8111",
 		Handler: mux,
@@ -41,4 +46,27 @@ func rootHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	w.Write(body)
+}
+
+func signCsr(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+
+	reqBody, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	csr := &ca.CertificateSigningRequest{}
+	err = json.Unmarshal(reqBody, csr)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	w.WriteHeader(http.StatusAccepted)
+	fmt.Fprintf(w, "received")
 }
